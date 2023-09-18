a6c8a710475b039737d26a9088d11fe6615b0dc5
unefillette86 <142242424+unefillette86@users.noreply.github.com>
2023-09-18 22:34:58 +0300

    Update bot.js 2 часть

--- bot/bot.js
+++ bot/bot.js
@@ -2,34 +2,80 @@
 // @name         New Userscript
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  Bot for yandex
+// @description  Bot for bing
 // @author       Petushkova Evgeniya
-// @match        https://ya.ru/*
+// @match        https://www.bing.com/*
+// @match        https://www.napli.ru/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // ==/UserScript==
 
-
 let links = document.links
-let btn = document.querySelector(`button[type="submit"]`).click();
-let input = document.getElementsByName("text")[0];
-let keywords = ["10 самых популярных аниме",
-            "топ аниме-мультфильмов",
-            "самые любимые герои аниме"]
+let searchIconBtn = document.getElementById("search_icon");
+let inputBing = document.getElementById("sb_form_q");
+let keywords = ["10 ппулярных шрифтов Google",
+                "Отключение редакций и ревизий",
+                "Вывод произвольных типов записей и полей wp",
+                "Конвертация Notion в Obsidian",
+                "FFmpeg",
+                "VSCode плагины"];
 let keyword = keywords[getRandom(0, keywords.length)];
-if (btn != undefined) {
-    input.value = keyword;
-    btn.click;
-} else {
- for (let i = 0; i < links.length; i++) {
-        if (links[i].href.indexOf("blog.eldorado.ru") != -1){
-            console.log("Нашел строку " + links[i]);
-            links[i].click();
-            break;
+//keyword ="Конвертация чего-то в куда-то";
+//работа на главной странице
+if (searchIconBtn != undefined) {
+    let i = 0;
+    let timerId = setInterval(function() {
+        inputBing.value += keyword[i];
+        i++;
+        if (i == keyword.length){
+            clearInterval(timerId);
+            searchIconBtn.click();
         }
-    }
-}
+    }, 700)
 
-function getRandom(min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
-}
+    //Работаем на  странице поисковой выдачи
+    } else if (location.hostname == napli.ru) {
+        console.log("Мы на целевом сайте");
+        setInterval(() => {
+            let index = getRando(0, links.lenght);
+            if (getRandom(0, 101) >= 75) {
+                location.href =" https://www.bing.com/*"
+            }
+            if (links.length == 0) {
+                location.href =" https://www.napli.ru/";
+            }
+            if (links[index].href.indexOf("napli.ru") != -1) {
+                links[index].click()
+            }
+        }, getRandom(1000, 5000));
+    } else {
+        let nextBingPage = true;
+        for (let i = 0; i < links.length; i++) {
+            if (links[i].href.indexOf("napli.ru") != -1) {
+                console.log("Нашел нужный сайт" + links[i]);
+                let link = links[i];
+                nextBingPage = false;
+                setTimeout(() => { link.click();}, getRandom(2000, 4000))
+                break;
+            }
+        } 
+        let elementExist = setInterval(() => {
+            let elm = document.querySelector(`a[aria-label="Страница 3"]`);
+            if (elm != null) {
+                if (elm.innerText == 4){
+                    nextBingPage = false;
+                    location.href =" https://www.bing.com/*"
+                }
+                clearInterval(elementExist);
+            }
+        })
+        //если не нашли на первой странице выдачи, то листаем дальше
+        if (nextBingPage) {
+            setTimeout(() => {
+                document.querySelector(`a[title="Следующая страница"]`).click();
+            }, getRandom(3500, 5500))
+
+
+            function getRandom (min, max){
+                return Math.floor(Math.random() * (min, max) + min);
+            }
